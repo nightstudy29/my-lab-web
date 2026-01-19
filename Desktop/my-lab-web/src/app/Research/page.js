86af8675b9474bd82759da360d6ef33c7f71266e
Junmin Suh <junminsuh@gmail.com>
2026-01-19 17:00:43 +0900

    내용수정함

---
+++ Desktop/my-lab-web/src/app/Research/page.js
@@ -0,0 +1,8 @@
+export default function ResearchPage() {
+  return (
+    <div style={{ padding: '40px', maxWidth: '1000px', margin: '0 auto' }}>
+      <h1>Research Areas</h1>
+      <p>Research content will be updated soon.</p>
+    </div>
+  );
+}
