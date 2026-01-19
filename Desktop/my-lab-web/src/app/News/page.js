625680b5c7e3a6beaae5d25d588a4d8fd339dff1
Junmin Suh <junminsuh@gmail.com>
2026-01-19 16:59:10 +0900

    내용수정함

---
+++ Desktop/my-lab-web/src/app/News/page.js
@@ -0,0 +1,8 @@
+export default function NewsPage() {
+  return (
+    <div style={{ padding: '40px', maxWidth: '1000px', margin: '0 auto' }}>
+      <h1>News</h1>
+      <p>Latest news will be updated soon.</p>
+    </div>
+  );
+}
