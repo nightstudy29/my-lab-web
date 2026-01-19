c5d238913329ee3ce56747f9453d25f923e06ff4
Junmin Suh <junminsuh@gmail.com>
2026-01-19 16:55:02 +0900

    내용수정함

---
+++ Desktop/my-lab-web/src/app/Contact/page.js
@@ -0,0 +1,8 @@
+export default function ContactPage() {
+  return (
+    <div style={{ padding: '40px', maxWidth: '1000px', margin: '0 auto' }}>
+      <h1>Contact Us</h1>
+      <p>Contact information will be updated soon.</p>
+    </div>
+  );
+}
