315fc76a6b08b5daa5a4076022a490dcc0b2125e
Junmin Suh <junminsuh@gmail.com>
2026-01-19 16:57:14 +0900

    내용수정함

---
+++ Desktop/my-lab-web/src/app/Equipment/page.js
@@ -0,0 +1,8 @@
+export default function EquipmentPage() {
+  return (
+    <div style={{ padding: '40px', maxWidth: '1000px', margin: '0 auto' }}>
+      <h1>Equipment</h1>
+      <p>Laboratory equipment list will be updated soon.</p>
+    </div>
+  );
+}
