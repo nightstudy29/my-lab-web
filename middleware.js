b4c3cd46f1167568b1fe59bf68937bba1c513227
Junmin Suh <junminsuh@gmail.com>
2026-02-04 16:21:19 +0900

    내용수정함

--- middleware.js
+++ middleware.js
@@ -9,7 +9,7 @@ export function middleware(request) {
   const cspHeader = `
     default-src 'self';
     script-src 'self' 'nonce-${nonce}' https://www.googletagmanager.com https://www.google-analytics.com;
-    style-src 'self' 'nonce-${nonce}';
+    style-src 'self' 'unsafe-inline';
     img-src 'self' blob: data: https://www.google-analytics.com;
     font-src 'self' data:;
     object-src 'none';
