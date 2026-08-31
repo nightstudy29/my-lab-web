4e2a8e72c7da783bd72c58a4e71cf77f2a122ef6
Junmin Suh <junminsuh@gmail.com>
2026-09-01 01:46:13 +0900

    내용수정함

---
+++ src/app/lecture/page.js
@@ -0,0 +1,47 @@
+{
+  "semester": "2026-2",
+  "courses": [
+    {
+      "id": "course-1",
+      "name": "과목명1 (수정 필요)",
+      "materials": [
+        {
+          "id": "c1-2026-09-01-01",
+          "date": "2026-09-01",
+          "week": 1,
+          "title": "1주차: Introduction",
+          "type": "slide",
+          "driveUrl": "https://drive.google.com/file/d/REPLACE_WITH_FILE_ID/view"
+        }
+      ]
+    },
+    {
+      "id": "course-2",
+      "name": "과목명2 (수정 필요)",
+      "materials": [
+        {
+          "id": "c2-2026-09-01-01",
+          "date": "2026-09-01",
+          "week": 1,
+          "title": "1주차: Introduction",
+          "type": "slide",
+          "driveUrl": "https://drive.google.com/file/d/REPLACE_WITH_FILE_ID/view"
+        }
+      ]
+    },
+    {
+      "id": "course-3",
+      "name": "과목명3 (수정 필요)",
+      "materials": [
+        {
+          "id": "c3-2026-09-01-01",
+          "date": "2026-09-01",
+          "week": 1,
+          "title": "1주차: Introduction",
+          "type": "slide",
+          "driveUrl": "https://drive.google.com/file/d/REPLACE_WITH_FILE_ID/view"
+        }
+      ]
+    }
+  ]
+}
